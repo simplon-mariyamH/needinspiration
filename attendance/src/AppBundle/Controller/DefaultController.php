7c7104d07a3124a7b5fc8d19bf34c41e570a6063
simplon-sofianeD <dahmani.sof@gmail.com>
2017-01-19 10:32:25 +0100

    daily update

--- attendance/src/AppBundle/Controller/DefaultController.php
+++ attendance/src/AppBundle/Controller/DefaultController.php
@@ -12,8 +12,9 @@ use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 
 
-$_POST['email'] = "Test_email";
-$_POST['motdepasse'] = "Test_motdepasse";
+ $_POST['email'] = "Test_email";
+ $_POST['motdepasse'] = "Test_motdepasse";
+ $_POST["id"] = 1;
 
 class DefaultController extends Controller
 {
@@ -23,15 +24,16 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         $students = $this->getEntities('AppBundle:Login');
-        return var_dump($students);
+        return new Response(var_dump($students));
     }
     /**
      * @Route("/Login", name="Login")
      */
-    public function verifIdentifiant(Request $request)
+    public function checkId(Request $request)
     {
         if ($request) 
         {
+            if (isset($_POST["email"]) && isset($_POST["motdepasse"])){
             $email = $_POST["email"];
             $motdepasse = $_POST["motdepasse"];
             $repository = $this->getDoctrine()->getRepository('AppBundle:Login');
@@ -51,7 +53,7 @@ class DefaultController extends Controller
                     "server"=>"success", 
                     "user"=>$user];
                 return new Response(json_encode($response));
-
+                // METTRE SERIALIZER DANS USER ET CREE UNE FONCTION OBJECT TO JSON
                 // $serializer = new Serializer(array(new GetSetMethodNormalizer()), array('json' => new JsonEncoder()));
                 // $json = $serializer->serialize($eleve, 'json');
                 
@@ -60,11 +62,44 @@ class DefaultController extends Controller
                 echo $response;
                 return new Response($json);
                 */
-              }      
+              }
+            } else {
+                return new Response("email ou mot de passe manquant");
+            }      
         } 
        
 
     }
+       /**
+     * @Route("/Signin", name="Signin")
+     */
+     public function signIn(Request $request)
+     {
+        // id verif si existe et ugrade la base de données. gerer si l'utilisateur a dejà signé.
+        if ($request) 
+        {
+            if(isset($_POST["id"])){
+                $id = $_POST["id"];
+                $repository = $this->getDoctrine()->getRepository('AppBundle:Login');
+                $eleve = $repository->findOneBy(
+                array("id"=>$id)
+                );
+                if(isset($eleve))
+                {
+                    return new Response("eleve existant");
+                } else {
+                    return new Response("élève inconnu");
+                }
+
+            } else {
+                return new Response("ID manquant");
+            }
+
+        }
+
+
+        return new Response();
+     } 
      private function getEntities( $entityType ):array
     {
         $entityManager = $this->getDoctrine()->getManager();
