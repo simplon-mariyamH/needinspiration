ae929b8a857efbd273ab3ab9c7c83c74033841af
simplon-mariyamH <mariyam.hamdaoui@gmail.com>
2017-01-23 18:49:44 +0100

    t

--- attendance/src/AppBundle/Controller/DefaultController.php
+++ attendance/src/AppBundle/Controller/DefaultController.php
@@ -25,6 +25,44 @@ class DefaultController extends Controller
         $students = $this->getEntities('AppBundle:Login');
         return new Response(var_dump($students));
     }
+
+    /**
+     * @Route("/Promo", name="Promo")
+     */
+    public function getHolidays($year = null)
+    {
+        if ($year === null)
+        {
+            $year = intval(date('Y'));
+        }
+
+        $easterDate  = easter_date($year);
+        $easterDay   = date('j', $easterDate);
+        $easterMonth = date('n', $easterDate);
+        $easterYear   = date('Y', $easterDate);
+
+        $holidays = array(
+            // Dates fixes
+            mktime(0, 0, 0, 1,  1,  $year),  // 1er janvier
+            mktime(0, 0, 0, 5,  1,  $year),  // Fête du travail
+            mktime(0, 0, 0, 5,  8,  $year),  // Victoire des alliés
+            mktime(0, 0, 0, 7,  14, $year),  // Fête nationale
+            mktime(0, 0, 0, 8,  15, $year),  // Assomption
+            mktime(0, 0, 0, 11, 1,  $year),  // Toussaint
+            mktime(0, 0, 0, 11, 11, $year),  // Armistice
+            mktime(0, 0, 0, 12, 25, $year),  // Noel
+
+            // Dates variables
+            mktime(0, 0, 0, $easterMonth, $easterDay + 1,  $easterYear),
+            mktime(0, 0, 0, $easterMonth, $easterDay + 39, $easterYear),
+            mktime(0, 0, 0, $easterMonth, $easterDay + 50, $easterYear),
+        );
+
+        sort($holidays);
+
+        return $holidays;
+    }
+
     /**
      * @Route("/Login", name="Login")
      */
